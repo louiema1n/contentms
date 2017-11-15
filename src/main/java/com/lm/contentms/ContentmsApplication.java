a16e5e2887ef556496c3b071afdf3a9764c7345c
louiemain <louiema1n@github.com>
2017-11-15 14:50:59 +0800

    Initial commit

---
+++ src/main/java/com/lm/contentms/ContentmsApplication.java
@@ -0,0 +1,12 @@
+package com.lm.contentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentmsApplication.class, args);
+	}
+}
